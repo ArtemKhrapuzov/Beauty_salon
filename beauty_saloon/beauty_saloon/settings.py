e1898dc01010f663fe3496c014b3d013df543d60
Artem Khrapuzov <tema102@mail.ru>
2023-10-04 21:17:25 +0600

    add html left bar

--- beauty_saloon/beauty_saloon/settings.py
+++ beauty_saloon/beauty_saloon/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'beauty_saloon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- beauty_saloon/service/views.py
+++ beauty_saloon/service/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+
+# class MainListView(ListView):
+#     template_name = 'service/index.html'
+
+def index(request):
+    return render(request, 'service/index.html')
