f16ba083a00b2db739f12be3837866ed58bb55db
Artem Khrapuzov <tema102@mail.ru>
2023-10-05 09:23:49 +0600

    add auth(login, logout)

--- beauty_saloon/service/urls.py
+++ beauty_saloon/service/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from service.views import *
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='home'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('register/', RegisterUser.as_view(), name='register')
 ]
--- beauty_saloon/service/views.py
+++ beauty_saloon/service/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.contrib.auth import logout, login
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
+
+from service.forms import RegisterUserForm, LoginUserForm
 
 
 # class MainListView(ListView):
@@ -7,3 +12,32 @@ from django.views.generic import ListView
 
 def index(request):
     return render(request, 'service/index.html')
+
+
+class RegisterUser(CreateView):
+    """Форма регистрации"""
+    form_class = RegisterUserForm
+    template_name = 'service/register.html'
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        """При успешной регистрации, переводит на главную страницу"""
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+
+class LoginUser(LoginView):
+    """Форма авторизации"""
+    forms_class = LoginUserForm
+    template_name = 'service/login.html'
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+def logout_user(request):
+    """Выход из авторизации"""
+    logout(request)
+    return redirect('login')
+
+
