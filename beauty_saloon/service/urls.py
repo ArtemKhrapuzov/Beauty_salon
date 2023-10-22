068bc2f9547ddc2ebc3b0a5cfcc9731c7346def8
Artem Khrapuzov <tema102@mail.ru>
2023-10-22 19:56:18 +0600

    add search html

--- beauty_saloon/service/urls.py
+++ beauty_saloon/service/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', index, name='home'),
     path('new_items/', NewProduct.as_view(), name='new_items'),
     path('catalog/<slug:subsub_slug>/', ProductList.as_view(), name='product_list_by_subsub'),
-    path('<str:slug>/', ProductDetail.as_view(), name='product_detail'),
+    path('cat/<str:slug>/', ProductDetail.as_view(), name='product_detail'),
 
 ]
